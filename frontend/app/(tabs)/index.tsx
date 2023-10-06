33b3047b58eb78b3aea6a8a4fd534f38e7c9c81f
bigya01 <106421905+bigya01@users.noreply.github.com>
2023-10-06 15:05:14 +0545

    fix:title bar

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,31 +1,79 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Image } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons'; 
+import playedData from "../../assets/data/played";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
+
+// import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* blocktitle */}
+      <View style={styles.titleblock}>
+        {/* tile */}
+        <View style={styles.texttitle}>
+          <Text style={styles.titlemain}>Welcome Back!</Text>
+          <Text style ={styles.titlesec}> Find today's best podcasts</Text>
+        </View>
+        {/* search button */}
+        <View style={styles.searchbtn}>
+        <EvilIcons name="search" size={24} color="white"  />
+        </View>
+      </View>
+      {/* played image */}
+      <View style={styles.played}>
+      </View>
+
+
+
     </View>
   );
 }
 
+
+
+
+
+
+
+
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#111111",
+    alignContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  titleblock: {
+    justifyContent: 'space-between',
+    marginTop:30,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  texttitle :{},
+  titlemain :{
+    paddingHorizontal: 20,
+    fontSize:24,
+    fontWeight: 'semi-bold',
+    color: "#FFFFFF"
   },
+  titlesec:{
+    paddingHorizontal: 20,
+    paddingTop: 10,
+    fontSize:14,
+    color :"#585858",
+  },
+  searchbtn:{
+    height:40,
+    width: 40,
+    backgroundColor: '#585858',
+    borderRadius:8,
+    justifyContent:'center',
+    alignItems:'center',
+    margin:5
+  },
+  
 });
---
+++ frontend/assets/data/played.js
@@ -0,0 +1,14 @@
+const playedData=[
+    {
+        id: 2,
+        image: require('../images/kumari.png'),
+        title: 'kumari',
+        selected: true,
+    },
+    {
+        id: 1,
+        image: require('../images/icon.png'),
+        title: 'Pizza',
+        selected: true,
+    }] 
+export default playedData;
